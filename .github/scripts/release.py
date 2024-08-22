6b6e06cff8286741bec3d7bb0f55a465448fdeda
Chintan Chitroda <112692919+chintanarya@users.noreply.github.com>
2024-08-22 18:05:15 +0530

    Update release.py

--- .github/scripts/release.py
+++ .github/scripts/release.py
@@ -41,7 +41,7 @@ def create_new_patch_release():
         new_version_number = bump_patch_number(last_version_number)
 
     subprocess.run(
-        ["gh", "release", "create", "--generate-notes", "0.0.15"],
+        ["gh", "release", "create", "--generate-notes", "0.0.16"],
         check=True,
     )
 
