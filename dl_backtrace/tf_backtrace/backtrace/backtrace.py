cbae1bf1a9da24a74e5951da9cb7020815c6621c
neerajaryaai <neeraj.singh@arya.ai>
2024-08-23 16:19:53 +0530

    feat: added support for residual layer

--- dl_backtrace/tf_backtrace/backtrace/backtrace.py
+++ dl_backtrace/tf_backtrace/backtrace/backtrace.py
@@ -447,7 +447,7 @@ class Backtrace(object):
                     )
                     all_wt[child_nodes[0]] += temp_wt
                 elif model_resource["graph"][start_layer]["class"] == 'Residual':
-                    temp_wt = UP.calculate_wt_add(
+                    temp_wt = UP.calculate_wt_residual(
                         all_wt[start_layer],
                         [all_out[ch] for ch in child_nodes],
                     )
