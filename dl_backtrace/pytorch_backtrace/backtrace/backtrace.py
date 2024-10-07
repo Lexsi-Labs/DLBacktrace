53f2c5bae1cce559c090683da381b4419b8de65f
neerajaryaai <neeraj.singh@arya.ai>
2024-10-07 18:42:56 +0530

    Add: support for parallelization of Self-Attention and Feed-Forward sub-modules of Transformer Architecture

--- dl_backtrace/pytorch_backtrace/backtrace/backtrace.py
+++ dl_backtrace/pytorch_backtrace/backtrace/backtrace.py
@@ -541,7 +541,7 @@ class Backtrace(object):
                     weights = all_wts[start_layer]
                     self_attention_weights = HP.rename_self_attention_keys(weights)
 
-                    temp_wt = UP.calculate_wt_self_attention(
+                    temp_wt = UP.calculate_wt_self_attention_parallel(
                         all_wt[start_layer],
                         all_out[child_nodes[0]][0].detach().numpy(),
                         self_attention_weights,
@@ -558,10 +558,12 @@ class Backtrace(object):
                 elif model_resource[1][start_layer]["class"] == 'Feed_Forward':
                     weights = all_wts[start_layer]
                     feed_forward_weights = HP.rename_feed_forward_keys(weights)
-                    temp_wt = UP.calculate_wt_feed_forward(
+                    config = self.model.config
+                    temp_wt = UP.calculate_wt_feed_forward_parallel(
                         all_wt[start_layer],
                         all_out[child_nodes[0]][0].detach().numpy(),
-                        feed_forward_weights
+                        feed_forward_weights, 
+                        config
                     )
                     all_wt[child_nodes[0]] += temp_wt
                     
