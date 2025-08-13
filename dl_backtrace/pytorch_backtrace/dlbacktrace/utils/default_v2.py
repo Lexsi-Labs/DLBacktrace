b0b5c694ac2115f9e7cb3984067b417fa2ef8267
Omkar Kakade <omkar.kakade@arya.ai>
2025-08-13 23:10:47 +0530

    bug fix

--- dl_backtrace/pytorch_backtrace/dlbacktrace/utils/default_v2.py
+++ dl_backtrace/pytorch_backtrace/dlbacktrace/utils/default_v2.py
@@ -141,10 +141,9 @@ def launch_self_attention(version, R_out, Q, K, V, masked_fill=None, scale=None,
     if version == 'original':
         return calculate_wt_self_attention_original(R_out, Q, K, V, masked_fill, scale, epsilon)
 
-    masked_fill_t = torch.tensor(masked_fill, dtype=torch.float32, device=device) if masked_fill is not None else None
-    
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     R_out_t, Q_t, K_t, V_t, scale_t = _prepare_tensors(device, R_out, Q, K, V, scale, epsilon)
+    masked_fill_t = torch.tensor(masked_fill, dtype=torch.float32, device=device) if masked_fill is not None else None
     
     if version == 'pytorch':
         result_torch = calculate_wt_self_attention_pytorch(R_out_t, Q_t, K_t, V_t, masked_fill_t, scale_t, epsilon)
