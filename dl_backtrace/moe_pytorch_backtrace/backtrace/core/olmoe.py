c73610903b4f2fab7143f0dd42565748d58f38eb
neerajaryaai <neeraj.singh@arya.ai>
2025-10-29 06:39:00 +0000

    update model_resource object as a python dict from list

--- dl_backtrace/moe_pytorch_backtrace/backtrace/core/olmoe.py
+++ dl_backtrace/moe_pytorch_backtrace/backtrace/core/olmoe.py
@@ -86,13 +86,13 @@ def build_olmoe_tree(model, root='jet_moe'):
     # reverse the layer_stack
     layer_stack = list(reversed(layer_stack))
 
-    model_resource = (layer_tree, ltree, outputs, inputs)
-    # model_resource = {
-    #     "layers": layer_tree,
-    #     "graph": ltree,
-    #     "outputs": outputs,
-    #     "inputs": inputs
-    # } 
+    # model_resource = (layer_tree, ltree, outputs, inputs)
+    model_resource = {
+        "layers": layer_tree,
+        "graph": ltree,
+        "outputs": outputs,
+        "inputs": inputs
+    } 
 
     return model_resource, layer_stack
 
