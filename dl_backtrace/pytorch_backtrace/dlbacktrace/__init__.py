918b316e3867bb04e6d52b8ebe4d794236d07e16
neerajaryaai <neeraj.singh@arya.ai>
2025-11-03 16:01:14 +0530

    fix: DLBacktraceFX to DLBacktrace

--- dl_backtrace/pytorch_backtrace/dlbacktrace/__init__.py
+++ dl_backtrace/pytorch_backtrace/dlbacktrace/__init__.py
@@ -6,7 +6,7 @@ from .aten_operations import *
 # Export pipeline modules (optional, can be imported separately)
 try:
     from .pipeline import DLBacktracePipeline, ModelRegistry, get_model_info, PipelineConfig
-    __all__ = ['DLBacktraceFX', 'activation_master', 'DLBacktracePipeline', 'ModelRegistry', 'get_model_info', 'PipelineConfig']
+    __all__ = ['DLBacktrace', 'activation_master', 'DLBacktracePipeline', 'ModelRegistry', 'get_model_info', 'PipelineConfig']
 except ImportError:
-    __all__ = ['DLBacktraceFX', 'activation_master']
+    __all__ = ['DLBacktrace', 'activation_master']
     
